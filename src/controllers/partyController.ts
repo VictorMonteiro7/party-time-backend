0cc21c696cca346c81cf3ed71e9493bed2ff42a3
Victor Monteiro <contato@ovictormonteiro.com>
2022-03-10 11:52:59 -0300

    Funções de pegar festas públicas e privadas e de deletar festa criadas

--- src/controllers/partyController.ts
+++ src/controllers/partyController.ts
@@ -1,7 +1,10 @@
+import bcrypt, { compare } from "bcrypt";
 import { Request, Response } from "express";
 import Party from "../models/Party";
 import { getUserByToken } from "../helpers/getUserByToken";
 import User from "../models/User";
+import fs from "fs";
+import path from "path";
 
 //Criar uma nova festa
 export const postParty = async (req: Request, res: Response) => {
@@ -82,8 +85,63 @@ export const getUserParty = async (req: Request, res: Response) => {
       return res.status(403).json({ error: "Não autorizado." });
     const user = (await getUserByToken(token, false)) as { _id: string };
     const party = await Party.findById({ _id: id, userId: user._id });
+    if (!party)
+      return res.status(400).json({ message: "Festa não encontrada!" });
     return res.status(200).json(party);
   } catch (err) {
     return res.status(400).json({ error: "Erro ao buscar festa." });
+  }
+};
+
+//Pegar festas públicas e privadas de acordo com o usuário logado.
+export const getPublicAndPrivateParties = async (
+  req: Request,
+  res: Response
+) => {
+  const [type, token] = (req.headers["authorization"] as string).split(" ");
+  if (type !== "Bearer")
+    return res.status(403).json({ error: "Não autorizado." });
+  try {
+    const user = (await getUserByToken(token, false)) as { _id: string };
+    const parties = await Party.find({ privacy: false });
+    const privateParties = await Party.find({
+      userId: user._id,
+      privacy: true,
+    });
+    if (parties.length === 0 && privateParties.length === 0)
+      return res.json({ message: "Nenhuma festa encontrada!" });
+    res.json({ parties, privateParties });
+  } catch (err) {
+    return res.status(401).json({ error: "Acesso Negado." });
+  }
+};
+
+//Deletar a festa do usuário logado.
+export const deleteParty = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const [type, token] = (req.headers["authorization"] as string).split(" ");
+    if (type !== "Bearer")
+      return res.status(403).json({ error: "Não autorizado." });
+    const user = (await getUserByToken(token, true)) as {
+      _id: string;
+      password: string;
+    };
+    if (!user) return res.status(403).json({ error: "Acesso negado!" });
+    const party = await Party.findById({ _id: id, userId: user._id });
+    if (!party) return res.status(400).json({ error: "Festa não encontrada!" });
+    const { password } = req.body;
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword)
+      return res.status(403).json({ error: "Acesso negado!" });
+    if (party.photos) {
+      party.photos.forEach((photo) => {
+        fs.unlinkSync(`./tmp/${photo}`);
+      });
+    }
+    await party.delete();
+    return res.status(200).json({ message: "Festa deletada com sucesso!" });
+  } catch (err) {
+    return res.status(403).json({ error: "Acesso negado." });
   }
 };
